0f1aa8e5f80639965f4923752fd13ebd1d80fffd
Lidia Fernández Oro <lfernandezoro@outlook.es>
2020-04-12 11:46:13 +0200

    EF Core SQLite app

---
+++ EFCoreSQLiteXamFormsApp/Models/DataBaseContext.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Xamarin.Forms;
+
+namespace EFCoreSQLiteXamFormsApp.Models
+{
+    public class DataBaseContext: DbContext, IDbContext
+    {
+        private IDatabaseService DataBaseService => DependencyService.Get<IDatabaseService>();
+
+        DataBaseContext _dbContext;
+        DataBaseContext IDbContext.DbContext
+        {
+            get
+            {
+                if (_dbContext == null)
+                {
+                    _dbContext = new DataBaseContext();
+                    _dbContext.Database.EnsureCreated();
+                }
+                return _dbContext;
+            }
+        }
+
+        public DbSet<Patient> Patients { get; set; }
+        public DbSet<Doctor> Doctors { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var j = DataBaseService.GetDbPath();
+            optionsBuilder.UseSqlite($"Filename={DataBaseService.GetDbPath()}");
+        }
+    }
+
+}
